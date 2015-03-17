4816b65ef94029999a7c07c84e4bd6a46f0e41e7
사용자 <@computer>
2015-03-17 15:23:09 +0900

    .

---
+++ JavaProject/src/org/dimigo/basic/Promotion.java
@@ -0,0 +1,21 @@
+package org.dimigo.basic;
+
+public class Promotion {
+
+	public static void main(String[] args) {
+		
+		int money = 1700000;
+		int workersu = 3;
+		int shopsu = 1500;
+		
+		System.out.println("<< 디미베네 연간 인건비 >>");
+		System.out.println("월 평균 급여 : " + String.format("%,d", 1700000) + "원");
+		System.out.println("점포 내 직원 수 : 3명");
+		System.out.println("점포 수 : " + String.format("%,d", 1500) + "개");
+		
+		System.out.println("연간 인건비 : " + String.format("%,d", 1700000) + "원");
+		
+
+	}
+
+}
