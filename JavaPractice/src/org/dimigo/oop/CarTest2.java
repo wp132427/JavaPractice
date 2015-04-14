1e4dcd2f6f0accadad6c40b742b3f47c4af39669
사용자 <@computer>
2015-04-14 15:25:39 +0900

    ㅌ

--- JavaPractice/src/org/dimigo/oop/CarTest2.java
+++ JavaPractice/src/org/dimigo/oop/CarTest2.java
@@ -5,8 +5,8 @@ public class CarTest2 {
 	public static void main(String[] args) {
 		
 		Car2 car = new Car2("현대자동차","제네시스","검정색",225,50000000);
-	    Car2 car2 = new Car2("기아자동차","K7","흰색",246,40000000);
-	    Car2 car3 = new Car2("삼성자동차","SM7","회색",200,38000000);
+		Car2 car2 = new Car2("기아자동차","K7","흰색",246,40000000);
+		Car2 car3 = new Car2("삼성자동차","SM7","회색",200,38000000);
 		
 		System.out.println("제조사명 : " + car.getCompany());
 		System.out.println("모델명 : " + car.getModel());
